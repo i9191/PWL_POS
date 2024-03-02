b1d948160b4d64a6990fbc275596dd1ca0ad7df8
Ibnu Tsalis Assalam <ibnutsalisassalam@gmail.com>
2024-03-02 07:59:35 +0700

    Pertemuan 3 - 4 Implementasi DB Facade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
@@ -18,18 +19,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class,'index']);
+Route::get('/level', [LevelController::class,'index']);
 Route::prefix('category')->group(function () {
     Route::get('/food-beverage', 
-        [ProductController::Class,'foodProductView']
+        [ProductController::class,'foodProductView']
     );
     Route::get('/beauty-health', 
-        [ProductController::Class,'beautyhealthProductView']
+        [ProductController::class,'beautyhealthProductView']
     );
     Route::get('/home-care', 
-        [ProductController::Class,'homeProductView']
+        [ProductController::class,'homeProductView']
     );
     Route::get('/baby-kid', 
-        [ProductController::Class,'babyProductView']
+        [ProductController::class,'babyProductView']
     );
 });
 Route::get('/user/{id}/name/{name}', [UserController::class,'profile']);
