2778c51c7f37a1ed0b7613109eb9b6ed58aeff8a
Ibnu Tsalis Assalam <ibnutsalisassalam@gmail.com>
2024-03-07 16:13:26 +0700

    Pertemuan 4 - 2.4 Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,8 +9,15 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $user = UserModel::where('level_id',3)->count();
-        // dd($user);
+        $user = UserModel::firstOrNew(
+            [
+                'username'=>'manager33',
+                'nama'=>'Manager Tiga TIga',
+                'password'=>Hash::make('12345'),
+                'level_id'=>2
+            ]
+        );
+        $user->save();
         return view('user', ['data' => $user]);
     }
     public function profile($id, $name) {
