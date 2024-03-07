57e0c83f0cd99fb367756258e620e72c2cb84d1b
Ibnu Tsalis Assalam <ibnutsalisassalam@gmail.com>
2024-03-07 13:46:12 +0700

    lupa push pertemuan 3 DB FACADE, QUERY BUILDER, ELOQUENT ORM

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,7 +6,22 @@
         <title>Profile</title>
     </head>
     <body>
-        <h2>ID: {{$id}}</h2>
-        <h2>NAME: {{strtoupper($name)}}</h2>
+        <h1>Data User</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+            </tr>
+            @foreach ($data as $u)
+                <tr>
+                    <td>{{ $u->user_id }}</td>
+                    <td>{{ $u->username }}</td>
+                    <td>{{ $u->nama }}</td>
+                    <td>{{ $u->level_id }}</td>
+                </tr>
+            @endforeach
+        </table>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
@@ -18,19 +20,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class,'index']);
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
 Route::prefix('category')->group(function () {
     Route::get('/food-beverage', 
-        [ProductController::Class,'foodProductView']
+        [ProductController::class,'foodProductView']
     );
     Route::get('/beauty-health', 
-        [ProductController::Class,'beautyhealthProductView']
+        [ProductController::class,'beautyhealthProductView']
     );
     Route::get('/home-care', 
-        [ProductController::Class,'homeProductView']
+        [ProductController::class,'homeProductView']
     );
     Route::get('/baby-kid', 
-        [ProductController::Class,'babyProductView']
+        [ProductController::class,'babyProductView']
     );
 });
-Route::get('/user/{id}/name/{name}', [UserController::class,'profile']);
+// Route::get('/user/{id}/name/{name}', [UserController::class,'profile']);
 Route::get('/transaction', [TransactionController::class,'transactionView']);
