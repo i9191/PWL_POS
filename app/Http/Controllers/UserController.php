ac2aca5a68b2aa425d1899719d9c2ad9d8e65f9e
Ibnu Tsalis Assalam <ibnutsalisassalam@gmail.com>
2024-03-07 16:34:06 +0700

    Pertemuan 4 - 2.5 Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,16 +9,22 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $user = UserModel::firstOrNew(
-            [
-                'username'=>'manager33',
-                'nama'=>'Manager Tiga TIga',
-                'password'=>Hash::make('12345'),
-                'level_id'=>2
-            ]
-        );
+        $user = UserModel::create([
+            'username'=>'manager11',
+            'nama'=>'Manager11',
+            'password'=>Hash::make('12345'),
+            'level_id'=>2
+        ]);
+        $user->username='manager12';
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username','level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama','username']));
+        
+        // return view('user', ['data' => $user]);
     }
     public function profile($id, $name) {
         $data['id'] = $id;
