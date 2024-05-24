806a9d5807949c3daa89fb6e5f1a4913ea11eaf6
Ibnu Tsalis Assalam <ibnutsalisassalam@gmail.com>
2024-05-24 10:03:49 +0700

    Jobsheet 11 - Praktikum Implementasi Eloquent Accessor

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -23,7 +24,7 @@ class UserModel extends Model implements Authenticatable, JWTSubject
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'image'];
 
     // Implement the Authenticatable methods
     public function getAuthIdentifier()
@@ -58,5 +59,11 @@ class UserModel extends Model implements Authenticatable, JWTSubject
 
     public function level() : BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    protected function image(): Attribute{
+        return Attribute::make(
+            get: fn ($image) => url('storage/posts/'.$image),
+        );
     }
 }
